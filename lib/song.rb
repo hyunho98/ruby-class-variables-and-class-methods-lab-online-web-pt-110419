f3b0503eb6a7b0bba87d2d795395024054cbc989
Alex Lee <hyunho98@gmail.com>
2019-12-03 22:46:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  @@count = 0
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @@count += 1
+    self.name = name
+    self.song = song
+    self.artist = artist
+  end
+
+
+
+end
