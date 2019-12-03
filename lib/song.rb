dc5c52ec61fdd5e7feaef9a40247cdc997df03d0
Alex Lee <hyunho98@gmail.com>
2019-12-03 22:50:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,17 @@
 class Song
   @@count = 0
-
+  @@genres = []
+  
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @@count += 1
     self.name = name
-    self.song = song
     self.artist = artist
+    self.genre = genre
+    if @@genres.include?(genre) == false
+      @@genres << genre
+    end
   end
 
   def count
